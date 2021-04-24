d7207c9ee91f13749345d457a7b20c8754a58682
eliabevces <38759694+eliabevces@users.noreply.github.com>
2021-04-24 16:55:16 -0300

    nlw end

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,3 +1,7 @@
+import 'package:DevQuiz/challenge/challenge_page.dart';
+import 'package:DevQuiz/core/app_colors.dart';
+import 'package:DevQuiz/home/home_controller.dart';
+import 'package:DevQuiz/home/home_state.dart';
 import 'package:DevQuiz/home/widgets/appbar/app_bar_widget.dart';
 import 'package:DevQuiz/home/widgets/level_button/level_button_widget.dart';
 import 'package:DevQuiz/home/widgets/quiz_card/quiz_card_widget.dart';
@@ -10,51 +14,88 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final controller = HomeController();
+
+  @override
+  void initState() {
+    super.initState();
+    controller.getUser();
+    controller.getQuizzes();
+    controller.stateNotifier.addListener(() {
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBarWidget(),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            children: [
-              SizedBox(
-                height: 24,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  LevelButtonWidget(
-                    label: "Fácil",
-                  ),
-                  LevelButtonWidget(
-                    label: "Médio",
-                  ),
-                  LevelButtonWidget(
-                    label: "Difícil",
-                  ),
-                  LevelButtonWidget(
-                    label: "Perito",
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 24,
-              ),
-              Expanded(
-                child: GridView.count(
-                  crossAxisSpacing: 16,
-                  mainAxisSpacing: 16,
-                  crossAxisCount: 2,
+    if (controller.state == HomeState.success) {
+      return Scaffold(
+          appBar: AppBarWidget(
+            user: controller.user!,
+          ),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 19),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 24,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    QuizCardWidget(),
-                    QuizCardWidget(),
-                    QuizCardWidget()
+                    LevelButtonWidget(
+                      label: "Fácil",
+                    ),
+                    LevelButtonWidget(
+                      label: "Médio",
+                    ),
+                    LevelButtonWidget(
+                      label: "Difícil",
+                    ),
+                    LevelButtonWidget(
+                      label: "Perito",
+                    ),
                   ],
                 ),
-              )
-            ],
+                SizedBox(
+                  height: 24,
+                ),
+                Expanded(
+                  child: GridView.count(
+                    crossAxisSpacing: 16,
+                    mainAxisSpacing: 16,
+                    crossAxisCount: 2,
+                    children: controller.quizzes!
+                        .map((e) => QuizCardWidget(
+                              title: e.title,
+                              completed:
+                                  "${e.questionAnswered}/${e.questions.length}",
+                              percent: e.questionAnswered / e.questions.length,
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => ChallengePage(
+                                            questions: e.questions,
+                                            title: e.title,
+                                          )),
+                                );
+                              },
+                            ))
+                        .toList(),
+                  ),
+                )
+              ],
+            ),
+          ));
+    } else {
+      return Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(AppColors.darkGreen),
           ),
-        ));
+        ),
+      );
+    }
   }
 }
--- lib/home/widgets/appbar/app_bar_widget.dart
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -1,10 +1,12 @@
 import 'package:DevQuiz/core/app_gradients.dart';
 import 'package:DevQuiz/core/app_text_styles.dart';
 import 'package:DevQuiz/home/widgets/score_card/score_card_widget.dart';
+import 'package:DevQuiz/shared/models/user_model.dart';
 import 'package:flutter/material.dart';
 
 class AppBarWidget extends PreferredSize {
-  AppBarWidget()
+  final UserModel user;
+  AppBarWidget({required this.user})
       : super(
             preferredSize: Size.fromHeight(250),
             child: Container(
@@ -24,7 +26,7 @@ class AppBarWidget extends PreferredSize {
                             style: AppTextStyles.title,
                             children: [
                               TextSpan(
-                                text: "Eliabe",
+                                text: user.name,
                                 style: AppTextStyles.titleBold,
                               )
                             ])),
@@ -34,14 +36,16 @@ class AppBarWidget extends PreferredSize {
                           decoration: BoxDecoration(
                               borderRadius: BorderRadius.circular(10),
                               image: DecorationImage(
-                                  image: NetworkImage(
-                                      'https://avatars.githubusercontent.com/u/38759694?v=4'))),
+                                  image: NetworkImage(user.photoUrl))),
                         )
                       ],
                     ),
                   ),
                   Align(
-                      alignment: Alignment(0.0, 1.0), child: ScoreCardWidget())
+                      alignment: Alignment(0.0, 1.0),
+                      child: ScoreCardWidget(
+                        percent: user.score / 100,
+                      ))
                 ],
               ),
             ));
