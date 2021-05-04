5b38f2c224daaa84aa8933abbc3bd2759ed39439
eliabevces <38759694+eliabevces@users.noreply.github.com>
2021-05-04 10:35:32 -0300

    adding more example quizzes

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -30,64 +30,68 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     if (controller.state == HomeState.success) {
       return Scaffold(
-          appBar: AppBarWidget(
-            user: controller.user!,
-          ),
-          body: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 19),
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 24,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    LevelButtonWidget(
-                      label: "Fácil",
-                    ),
-                    LevelButtonWidget(
-                      label: "Médio",
-                    ),
-                    LevelButtonWidget(
-                      label: "Difícil",
-                    ),
-                    LevelButtonWidget(
-                      label: "Perito",
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 24,
-                ),
-                Expanded(
-                  child: GridView.count(
-                    crossAxisSpacing: 16,
-                    mainAxisSpacing: 16,
-                    crossAxisCount: 2,
-                    children: controller.quizzes!
-                        .map((e) => QuizCardWidget(
-                              title: e.title,
-                              completed:
-                                  "${e.questionAnswered}/${e.questions.length}",
-                              percent: e.questionAnswered / e.questions.length,
-                              onTap: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => ChallengePage(
-                                            questions: e.questions,
-                                            title: e.title,
-                                          )),
-                                );
-                              },
-                            ))
-                        .toList(),
+        appBar: AppBarWidget(
+          user: controller.user!,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 19),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 24,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  LevelButtonWidget(
+                    label: "Fácil",
                   ),
-                )
-              ],
-            ),
-          ));
+                  LevelButtonWidget(
+                    label: "Médio",
+                  ),
+                  LevelButtonWidget(
+                    label: "Difícil",
+                  ),
+                  LevelButtonWidget(
+                    label: "Perito",
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              Expanded(
+                child: GridView.count(
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                  crossAxisCount: 2,
+                  children: controller.quizzes!
+                      .map(
+                        (e) => QuizCardWidget(
+                          title: e.title,
+                          completed:
+                              "${e.questionAnswered}/${e.questions.length}",
+                          percent: e.questionAnswered / e.questions.length,
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => ChallengePage(
+                                  questions: e.questions,
+                                  title: e.title,
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      )
+                      .toList(),
+                ),
+              )
+            ],
+          ),
+        ),
+      );
     } else {
       return Scaffold(
         body: Center(
